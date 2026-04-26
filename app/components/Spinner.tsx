5aeea4f486f12e8d7a564752e20b06b24dc04c6a
unknown <theolowuayo@gmail.com>
2026-04-25 20:39:50 -0600

    fixed page loading css and script  and imported asset files to layout

---
+++ app/components/Spinner.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="page-loading active">
+      <div className="page-loading-inner">
+        <div className="page-spinner"></div>
+        <span>Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
