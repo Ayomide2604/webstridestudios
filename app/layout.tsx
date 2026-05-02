05a87831937a2e2c1e269472623a557294980668
Ayomide2604 <Theolowuayo@gmail.com>
2026-05-02 16:16:53 -0600

    Added and Fixed favicon updated social icons on hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en" data-bs-theme="light">
       <head>
+        {/* Favicons */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        
+        {/* Stylesheets */}
         <link
           rel="stylesheet"
           media="screen"
